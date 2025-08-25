72953e5336d3d4c1b151825d2bfe68c8ebca55af
njelich <12912633+njelich@users.noreply.github.com>
2025-08-25 12:21:59 +0200

    feat: implemented standard types

--- src/packed.rs
+++ src/packed.rs
@@ -1,6 +1,8 @@
 //! Module implementing packed encoding.
 
-use crate::encode::BufferSizeError;
+
+use crate::encode::{BufferSizeError};
+use std::borrow::Cow;
 
 pub trait EncodePacked {
     fn encode_packed(&self, out: &mut [u8]);
@@ -47,7 +49,19 @@ impl_encode_packed_for_integer!(
     i8, i16, i32, i64, i128, isize,
 );
 
-impl<T, const N: usize> EncodePacked for [T; N]
+macro_rules! impl_encode_packed_for_ref {
+    () => {
+        fn packed_size(&self) -> usize {
+            (**self).packed_size()
+        }
+
+        fn encode_packed(&self, out: &mut [u8]) {
+            (**self).encode_packed(out)
+        }
+    };
+}
+
+impl<T> EncodePacked for [T]
 where
     T: EncodePacked,
 {
@@ -65,17 +79,130 @@ where
     }
 }
 
-macro_rules! impl_encode_packed_for_ref {
-    () => {
-        fn packed_size(&self) -> usize {
-            (**self).packed_size()
+impl<T, const N: usize> EncodePacked for [T; N]
+where
+    T: EncodePacked,
+{
+    fn packed_size(&self) -> usize {
+        self.as_slice().packed_size()
+    }
+
+    fn encode_packed(&self, out: &mut [u8]) {
+        self.as_slice().encode_packed(out)
+    }
+}
+
+
+impl<T, const N: usize> EncodePacked for &'_ [T; N]
+where
+    T: EncodePacked,
+{
+    impl_encode_packed_for_ref!();
+}
+
+impl<T> EncodePacked for Vec<T>
+where
+    T: EncodePacked,
+{
+    impl_encode_packed_for_ref!();
+}
+
+impl EncodePacked for str {
+    fn packed_size(&self) -> usize {
+        self.as_bytes().packed_size()
+    }
+
+    fn encode_packed(&self, out: &mut [u8]) {
+        self.as_bytes().encode_packed(out)
+    }
+}
+
+impl EncodePacked for &'_ str {
+    impl_encode_packed_for_ref!();
+}
+
+impl EncodePacked for String {
+    impl_encode_packed_for_ref!();
+}
+
+impl<T> EncodePacked for Cow<'_, T>
+where
+    T: EncodePacked + ToOwned + ?Sized,
+{
+    fn packed_size(&self) -> usize {
+        self.as_ref().packed_size()
+    }
+
+    fn encode_packed(&self, out: &mut [u8]) {
+        self.as_ref().encode_packed(out)
+    }
+}
+
+macro_rules! impl_encode_packed_for_tuple {
+    ($($t:ident),*) => {
+        #[allow(non_snake_case, unused_variables)]
+        impl<$($t),*> EncodePacked for ($($t,)*)
+        where
+            $($t: EncodePacked,)*
+        {
+            fn packed_size(&self) -> usize {
+                let ($($t,)*) = self;
+                0 $(+ $t.packed_size())*
+            }
+
+            fn encode_packed(&self, out: &mut [u8]) {
+                let ($($t,)*) = self;
+                let mut offset = 0;
+                $(
+                    let end = offset + $t.packed_size();
+                    $t.encode_packed(&mut out[offset..end]);
+                    offset = end;
+                )*
+            }
         }
 
-        fn encode_packed(&self, out: &mut [u8]) {
-            (**self).encode_packed(out)
+        impl<$($t),*> EncodePacked for &'_ ($($t,)*)
+        where
+            $($t: EncodePacked,)*
+        {
+            impl_encode_packed_for_ref!();
         }
     };
 }
+
+impl_encode_packed_for_tuple! {}
+impl_encode_packed_for_tuple! { A }
+impl_encode_packed_for_tuple! { A, B }
+impl_encode_packed_for_tuple! { A, B, C }
+impl_encode_packed_for_tuple! { A, B, C, D }
+impl_encode_packed_for_tuple! { A, B, C, D, E }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE }
+impl_encode_packed_for_tuple! { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF }
 
 #[cfg(test)]
 mod tests {
@@ -100,8 +227,14 @@ mod tests {
         assert_eq!(encode_packed(&-128i8), hex!("80"));
         assert_eq!(encode_packed(&127i8), hex!("7f"));
 
-        // Tests for array
-
+        // Tests for arra
         assert_eq!(encode_packed(&[1u8, 2, 3]), hex!("010203"));
+
+        // Tests for strings and variants
+        assert_eq!(encode_packed(&"hello"), hex!("68656c6c6f"));
+
+        // Tests for tuples
+        assert_eq!(encode_packed(&(1u8, 2u8)), hex!("0102"));
+        assert_eq!(encode_packed(&(1u8, 2u8, 3u8)), hex!("010203"));
     }
 }
